99b7438f3a3eacbf16fa8ec80f41eec6ac459094
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-06-06 15:59:29 +0200

    code optimization

---
+++ html-css-js/slider2/Js/main.js
@@ -0,0 +1,65 @@
+
+
+let currentList = 1;
+let cardLists = document.querySelectorAll(".cards-list");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+let paginationitems = document.querySelectorAll(".pagination .pagi-item");
+
+checker();
+nextBtn.onclick = () => {
+    if(currentList < 3)
+    {currentList += 1;
+        checker();
+    }
+}
+
+prevBtn.onclick = () => {
+    if(currentList > 1)
+    {
+        currentList -= 1;
+        checker();
+    }
+}
+
+
+function checker() {
+    
+        cardLists.forEach((el) => {
+            el.classList.remove("active");
+        })
+    cardLists[currentList - 1].classList.add("active");
+    paginationitems.forEach((el) => {
+        el.classList.remove("active");
+    });
+    paginationitems[currentList - 1].classList.add("active");
+    
+        if (currentList == 1) {
+            prevBtn.classList.add("disable");
+            nextBtn.classList.add("active");
+
+    }
+        if (currentList == 2) {
+            prevBtn.classList.remove("disable");
+            prevBtn.classList.add("active");
+            nextBtn.classList.add("active");
+            nextBtn.classList.remove("disable");
+    }
+        if (currentList == 3) {
+            nextBtn.classList.remove("active");
+            nextBtn.classList.add("disable");
+    }
+
+}
+
+paginationitems.forEach((el) => {
+    el.addEventListener("click", (e) => {
+    for (let i = 0; i < paginationitems.length; i++) {
+        paginationitems[i].classList.remove("active");
+        } 
+        el.classList.add("active");  
+        currentList = e.currentTarget.id;
+        checker();
+    });
+});
+
